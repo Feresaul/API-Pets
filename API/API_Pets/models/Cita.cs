a62930b175a63faa60d64126d37d454ad641c167
Esaúl <f.esaul.franco.r@gmail.com>
2020-10-31 13:18:19 -0600

    API Update
    added agregarCita

---
+++ API/API_Pets/models/Cita.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.models
+{
+    public class Cita
+    {
+        public int idServicio { set; get; }
+        public int idUsuario { set; get; }
+        public string fechaInicio { set; get; }
+    }
+}
